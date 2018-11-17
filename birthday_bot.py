06cd8ffb91e9df17b6acda7aefd14c771265ffb9
Sviatoslav <sviatoslav.pereverziev@gmail.com>
2018-11-18 01:13:59 +0200

    Add keyboard

--- birthday_bot.py
+++ birthday_bot.py
@@ -1,6 +1,7 @@
 from settings_bot import TOKEN
 import telebot
 from telebot import types
+import calendar
 import time
 
 bot = telebot.TeleBot(TOKEN)
@@ -8,16 +9,44 @@ upd = bot.get_updates()
 new_user = False
 
 
-@bot.message_handler(commands=['start'])
+def keybord_y_or_n(message, text):
+    keyboard = types.InlineKeyboardMarkup()
+    button_yes = types.InlineKeyboardButton(text='Yes', callback_data='name_yes')
+    button_no = types.InlineKeyboardButton(text='No', callback_data='name_no')
+    keyboard.add(button_yes, button_no)
+    bot.send_message(message.chat.id, text, reply_markup=keyboard)
+
+
+def keybord_day(message, text, month):
+    keyboard = types.InlineKeyboardMarkup()
+    cal = calendar.Calendar(firstweekday=0)
+    day_in_month = []
+    button = {}
+    for day in cal.monthdays2calendar(2000, month):
+        day_in_month.append(day)
+
+    for week in range(len(day_in_month)):
+        print(week)
+        for day in day_in_month[week]:
+            print(day[0])
+
+
+    # for x in day_in_month[0]:
+    #     keybord.add(x)
+
+    bot.send_message(message.chat.id, text, reply_markup=keyboard)
+
+@ bot.message_handler(commands=['start'])
 def keybord(message):
-    markup = types.ReplyKeyboardMarkup(row_width=2, one_time_keyboard=True, resize_keyboard=True)
-    itembtn1 = types.KeyboardButton('/all')
-    itembtn2 = types.KeyboardButton('/week')
-    itembtn3 = types.KeyboardButton('/month')
-    itembtn4 = types.KeyboardButton('/add')
-    itembtn5 = types.KeyboardButton('/delete')
-    markup.add(itembtn1, itembtn2, itembtn3, itembtn4, itembtn5)
-    bot.send_message(message.chat.id, 'Choose command:', reply_markup=markup, )
+    keybord_day(message, 'smmm', 2)
+    # markup = types.ReplyKeyboardMarkup(row_width=2, one_time_keyboard=True, resize_keyboard=True)
+    # itembtn1 = types.KeyboardButton('/all')
+    # itembtn2 = types.KeyboardButton('/week')
+    # itembtn3 = types.KeyboardButton('/month')
+    # itembtn4 = types.KeyboardButton('/add')
+    # itembtn5 = types.KeyboardButton('/delete')
+    # markup.add(itembtn1, itembtn2, itembtn3, itembtn4, itembtn5)
+    # bot.send_message(message.chat.id, 'Choose command:', reply_markup=markup, )
 
 
 @bot.message_handler(commands=['all'])
@@ -53,11 +82,8 @@ def last_message(message):
     global new_user
     if new_user:
         new_user = False
-        keyboard = types.InlineKeyboardMarkup()
-        button_yes = types.InlineKeyboardButton(text='Yes', callback_data='name_yes')
-        button_no = types.InlineKeyboardButton(text='No', callback_data='name_no')
-        keyboard.add(button_yes, button_no)
-        bot.send_message(message.chat.id, '{}\nThis is the name? '.format(lm), reply_markup=keyboard)
+        text = '{}\nThis is the name? '.format(lm)
+        keybord_y_or_n(message, text)
 
 
 @bot.callback_query_handler(func=lambda call: True)
