6a0c8870f205847a4c06a4ce2bd9f8d4129b2ca6
Sviatoslav <sviatoslav.pereverziev@gmail.com>
2018-12-02 17:15:48 +0200

    Delete class User

--- birthday_bot.py
+++ birthday_bot.py
@@ -9,65 +9,6 @@ from mysql.connector import Error
 bot = telebot.TeleBot(TOKEN)
 
 
-class User(object):
-    def __init__(self, from_user):
-        self.id = from_user.id
-        self.is_bot = from_user.is_bot
-        self.first_name = from_user.first_name
-        self.username = from_user.username
-        self.last_name = from_user.last_name
-        self.language_code = from_user.language_code
-
-    def get_user_info(self):
-        user_info = {'id': self.id, 'is_bot': self.is_bot, 'first_name': self.first_name, 'username': self.username,
-                     'last_name': self.last_name, 'language_code': self.language_code}
-        return user_info
-
-    def get_user_id(self):
-        return self.id
-
-    def add_user_information(self):
-        self.add_user_name = None
-        self.add_user_date = None
-        self.add_user_month = None
-
-    def set_add_user_name(self, name):
-        self.add_user_name = name
-
-    def set_add_user_date(self, date):
-        self.add_user_date = date
-
-    def set_add_user_month(self, month):
-        self.add_user_month = month
-
-    def get_add_user_name(self):
-        return self.add_user_name
-
-    def get_add_user_date(self):
-        return self.add_user_date
-
-    def get_add_user_month(self):
-        return self.add_user_month
-
-    def add_new_user(self):
-        self.add_new_user = False
-
-    def set_add_new_user(self, bool):
-        self.add_new_user = bool
-
-    def get_add_new_user(self):
-        return self.add_new_user
-
-    def last_update(self):
-        self.last_update = None
-
-    def set_last_update(self, update):
-        self.last_update = update
-
-    def get_last_update(self):
-        return self.last_update
-
-
 class ConnectDb(object):
     def connected(self):
         self.db = mysql.connector.connect(host='localhost',
@@ -148,21 +89,13 @@ class ConnectDb(object):
         mycursor.execute(sql, user)
         self.db.commit()
 
-    def get_birthday(self):
+    def get_birthday(self, id):
+        sql = 'SELECT name, month_str, day FROM Birthdays WHERE id = {}'.format(id)
         mycursor = self.db.cursor()
-        sql = 'SELECT name, month_str, day FROM Birthday_bot.id_{};'.format(user.get_user_id())
         mycursor.execute(sql)
         myresult = mycursor.fetchall()
         return myresult
 
-    # def create_user_db(self, id):
-    #     try:
-    #         mycursor = self.db.cursor()
-    #         sql = 'CREATE TABLE id_{} (name VARCHAR(255), month_int INTEGER(2), month_str VARCHAR(25), day INTEGER(2), year_of_birth INTEGER(4), remind_month BOOL DEFAULT 0, remind_week BOOL DEFAULT 0, remind_day BOOL DEFAULT 0)'.format(
-    #             id)
-    #         mycursor.execute(sql)
-    #     except mysql.connector.errors.ProgrammingError:
-    #         print('Таблица id_{} уже есть!'.format(id))
 
 
 db = ConnectDb()
@@ -187,17 +120,11 @@ def commands(message):
 
 @bot.message_handler(commands=['all'])
 def all_birthdays(message):
-    pass
-    # global user, db
-    # user = User(message.from_user)
-    # db = ConnectDb()
-    # db.connected()
-    # bot.send_message(message.chat.id, 'Все дни рождения:')
-    # birthdays = db.get_birthday()
-    # for result in birthdays:
-    #     bot.send_message(message.chat.id,
-    #                      '{}'.format(
-    #                          str(result).replace(',', '').replace("'", '').replace('(', '').replace(')', '')))
+    birthdays = db.get_birthday(message.from_user.id)
+    for result in birthdays:
+        bot.send_message(message.chat.id,
+                         '{}'.format(
+                             str(result).replace(',', '').replace("'", '').replace('(', '').replace(')', '')))
 
 
 @bot.message_handler(commands=['week'])
@@ -215,15 +142,6 @@ def month_birthdays(message):
 @bot.message_handler(commands=['add'])
 def add_user(message):
     db.set_addition_data('add_user', '1', message.from_user.id)
-    # global user, db
-    # try:
-    #     # get user scenario status from db or Global Static Config Class
-    #     user.set_add_new_user(True)
-    # except NameError:
-    #     user = User(message.from_user)
-    #     # db = ConnectDb()
-    #     user.set_add_new_user(True)
-
     bot.send_message(message.chat.id, 'Добавим нового именинника: \nНапиши кто именинник?')
 
 
@@ -252,16 +170,16 @@ def callback_inline(call):
     value = call.data.split('_')[1]
     if command == 'answer':
         if value == 'yes':
-                db.add_birthday(call.message.chat.id)
-                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
-                                      text='Добавил 😌')
+            db.add_birthday(call.message.chat.id)
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                  text='Добавил 😌')
 
 
         elif value == 'no':
+            db.set_addition_data('add_user', '1', call.message.chat.id)
             bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
                                   text='Давай по новой \nТак кто именинник?')
-            global new_user
-            new_user = True
+
 
     elif command == 'month':
         bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
